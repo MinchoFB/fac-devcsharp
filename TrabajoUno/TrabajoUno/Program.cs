98578b241204130d6c3e3ef7aa11b311b8b68910
Fermin Bueno <minchofb14@gmail.com>
2013-04-29 19:51:08 -0300

    Inicio del Proyecto

---
+++ TrabajoUno/TrabajoUno/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Trabajo N° 1. 
+ * Author: Fermin Bueno	
+ * 
+ */
+using System;
+
+namespace TrabajoUno
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
